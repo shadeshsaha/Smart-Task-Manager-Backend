e8e5e1944ec3170569b409a205d4107f17218b08
shadeshsaha <shadesh15-8599@diu.edu.bd>
2025-11-22 01:49:19 +0600

    update: Updated Task Route for Upload & Validation

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -5,9 +5,28 @@ import {
   getTasks,
 } from "../controllers/taskController";
 import { authenticate } from "../middlewares/auth";
+import { upload } from "../middlewares/upload";
+import { validate } from "../middlewares/validate";
+import {
+  autoAssignSchema,
+  createTaskSchema,
+} from "../validations/taskValidation";
 
 export const taskRouter = Router();
 
 taskRouter.post("/", authenticate, createTask);
 taskRouter.get("/", authenticate, getTasks);
 taskRouter.post("/auto-assign", authenticate, autoAssignTask);
+taskRouter.post(
+  "/",
+  authenticate,
+  upload.single("file"),
+  validate(createTaskSchema),
+  createTask
+);
+taskRouter.post(
+  "/auto-assign",
+  authenticate,
+  validate(autoAssignSchema),
+  autoAssignTask
+);
